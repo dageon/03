f12898d595cb6345ab8eaa786373da1a8c8c4fc1
dageon <93782184+dageon@users.noreply.github.com>
2023-09-21 16:27:13 +0900

    update 4

--- main.c
+++ main.c
@@ -3,15 +3,15 @@
 
 
 int main(int argc, char *argv[]) {
-	float a, b;
+	int a, b;
 	
 	printf("분자를 입력하세요 : ");
-	scanf("%f", &a);
+	scanf("%d", &a);
 	printf("분모를 입력하세요 : ");
-	scanf("%f", &b);
+	scanf("%d", &b);
 	
 	
-	printf("나누기의 결과는 %f 입니다.\n", a/b);
+	printf("나누기의 결과는 %f 입니다.\n", (float)a/b);
 	
 	return 0;
 }
